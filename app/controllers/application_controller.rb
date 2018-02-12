b9d9468146a76ac1bf07c7690b9dc9a243a2c43d
Brian Biggers <brian1073@hotmail.com>
2018-02-11 21:30:44 -0500

    added a root rout and render html message for testing deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "This is going to be awesome! Good job me!"
+  end
 end
